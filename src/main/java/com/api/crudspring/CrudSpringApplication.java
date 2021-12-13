c861b37b26b562e7818e731a7192651fb169c6c9
opina-ai <kayque-willy@hotmail.com>
2021-12-13 20:39:24 -0300

    Remoção do código antigo

--- src/main/java/com/api/crudspring/CrudSpringApplication.java
+++ src/main/java/com/api/crudspring/CrudSpringApplication.java
@@ -1,12 +1,7 @@
 package com.api.crudspring;
 
-// import com.api.crudspring.model.Course;
-// import com.api.crudspring.repository.CourseRepository;
-
-// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -15,15 +10,4 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
-	// Temporário
-	// @Bean
-	// CommandLineRunner initDatabase(CourseRepository courseRepository) {
-	// 	return args ->{
-	// 		courseRepository.deleteAll();
-	// 		Course c = new Course();
-	// 		c.setName("Nome");
-	// 		c.setCategory("Descrição");
-	// 		courseRepository.save(c);
-	// 	};
-	// }
 }
